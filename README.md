f917008ecada31de1fa612e018dffd7b26c8c1b0
Kevin Burk <xavierholt@gmail.com>
2022-05-24 20:55:11 -0700

    SMILES Viewer

---
+++ README.md
@@ -0,0 +1,12 @@
+# Web Tools
+
+## SMILES Viewer
+
+Type a SIMLES string and see the molecular structure.\
+<https://acelot-inc.github.com/web-tools/smiles-viewer.html>
+
+
+## External Libraries
+
+Built with Smiles Drawer (MIT License):\
+<https://github.com/reymond-group/smilesDrawer>
---
+++ smiles-viewer.js
@@ -0,0 +1,45 @@
+// https://github.com/reymond-group/smilesDrawer
+
+drawer = null
+
+function onresize() {
+  const wrapper = document.getElementById('wrapper')
+  drawer = new SmilesDrawer.Drawer({
+    width:  wrapper.offsetWidth,
+    height: wrapper.offsetHeight
+  })
+}
+
+function onsubmit(redraw) {
+  const smiles = document.getElementById('smiles')
+
+  SmilesDrawer.parse(smiles.value, function(tree) {
+    smiles.classList.remove('invalid')
+    if(redraw) {
+      drawer.draw(tree, 'canvas', 'light')
+    }
+  }, function(error) {
+    smiles.classList.add('invalid')
+    console.log(error)
+  })
+}
+
+window.onload = function() {
+  window.addEventListener('resize', function(event) {
+    onresize()
+  })
+
+  document.getElementById('form').addEventListener('submit', function(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    onsubmit(true)
+  })
+
+  document.getElementById('smiles').addEventListener('keyup', function(event) {
+    const auto = document.getElementById('auto')
+    onsubmit(auto.checked)
+  })
+
+  onresize()
+  onsubmit()
+}
